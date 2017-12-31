b1f91d0d84641d67422575e344461ff8cda88c32
dhanyn10 <dhanyn.dhan@gmail.com>
2017-12-31 18:48:47 +0700

    alert javascript

---
+++ js/script.js
@@ -0,0 +1,10 @@
+(function(){
+    //alert
+    jalert = document.getElementsByClassName("alert-close");
+    for(a = 0; a < jalert.length; a++)
+    {
+        jalert[a].addEventListener('click', function(){
+            this.parentElement.style.display = "none";
+        });
+    }
+})();
